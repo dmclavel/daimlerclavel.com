1ff481e6e7c182d8880d9840f436f0b9a352099c
Daimler Clavel <daimler@spiralytics.com>
2021-07-11 11:35:16 +0800

    Utilized useMediaQuery hook for HeroSection breakpoint

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import useMediaQuery from '../../utils/hooks/mediaQuery';
 
 const HeroSection = () => {
-  const isBreakpoint =
-    (window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth) < 1024;
-
+  const isBreakpoint = useMediaQuery('(max-width: 1024px)');
   const handleClick = () => {
     document.getElementById('contact-heading-block').scrollIntoView();
   };
@@ -13,37 +10,37 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col py-16 lg:py-28 xl:py-36 2xl:py-44 2xl:mx-44">
       <p
-        style={{ animationDelay: isBreakpoint ? '400ms' : '1300ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '400ms' : '1300ms' }}
         className="tracking-wider font-semibold opacity-0 mb-2 lg:mb-0 text-blue dark:text-mint-light animate-up"
       >
         Hi there! I am
       </p>
       <p
-        style={{ animationDelay: isBreakpoint ? '500ms' : '1400ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '500ms' : '1400ms' }}
         className="tracking-wider font-bold opacity-0 name-highlight text-black-semi dark:text-white-emphasis animate-up"
       >
         Daimler Clavel.
       </p>
       <p
-        style={{ animationDelay: isBreakpoint ? '600ms' : '1500ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '600ms' : '1500ms' }}
         className="mb-4 opacity-0 description-dim text-black-disabled dark:text-white-disabled animate-up"
       >
         I love coding web-related stuff.
       </p>
       <p
-        style={{ animationDelay: isBreakpoint ? '700ms' : '1600ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '700ms' : '1600ms' }}
         className="opacity-0 text-base text-black dark:text-white-emphasis lg:text-lg animate-up"
       >
         Front-end Web Developer
       </p>
       <p
-        style={{ animationDelay: isBreakpoint ? '800ms' : '1600ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '800ms' : '1600ms' }}
         className="opacity-0 text-base text-black dark:text-white-emphasis lg:text-lg animate-up"
       >
         Data Analyst
       </p>
       <p
-        style={{ animationDelay: isBreakpoint ? '900ms' : '1600ms' }}
+        style={{ animationDelay: isBreakpoint.matches ? '900ms' : '1600ms' }}
         className="opacity-0 text-base text-black dark:text-white-emphasis lg:text-lg animate-up"
       >
         CRO Practitioner
@@ -51,7 +48,7 @@ const HeroSection = () => {
       <div
         style={{
           width: 'fit-content',
-          animationDelay: isBreakpoint ? '1000ms' : '1700ms',
+          animationDelay: isBreakpoint.matches ? '1000ms' : '1700ms',
         }}
         className="inline-block relative mt-10 opacity-0 animate-up cursor-pointer"
       >
@@ -62,7 +59,7 @@ const HeroSection = () => {
           Contact Me
         </button>
         <div
-          style={{ animationDelay: isBreakpoint ? '1300ms' : '2000ms' }}
+          style={{ animationDelay: isBreakpoint.matches ? '1300ms' : '2000ms' }}
           className="absolute -bottom-1 left-0 h-0.5 bg-blue dark:bg-mint-light animate-block-width-interpolate"
         />
       </div>
