33c66c5f1d46c728b738d0312cc0db418cd3ea45
Daimler Clavel <daimlerclavel@Daimlers-MacBook-Air.local>
2023-07-17 20:02:10 +0800

    Added new project in Projects section

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -20,32 +20,69 @@ function Projects() {
           boxId="project-box-1"
           projectTitle="InteReact: Online Classroom Portal for Language Learners &amp; Teachers"
           projectLink="https://intereact-online.netlify.app/"
-          content={(
+          content={
             <span className="font-sans text-sm text-black dark:text-white-semi tracking-wide">
-              This is a language learning platform which leverages the use of online clasroom
-              portals. Classroom features include Lectures, Assignments, Realtime Chat with
-              Translation feature, Live steam, Calendar and Certificate of Completion. This
-              application currently supports English and Italian languages. Application elements
-              ike dates, numbers, etc. are localized based on the language chosen.
+              This is a language learning platform which leverages the use of online
+              clasroom portals. Classroom features include Lectures, Assignments, Realtime
+              Chat with Translation feature, Live steam, Calendar and Certificate of
+              Completion. This application currently supports English and Italian
+              languages. Application elements ike dates, numbers, etc. are localized based
+              on the language chosen.
             </span>
-          )}
-          stack={['Mongo DB', 'Express.js', 'React.js', 'Node.js', 'Socket.io', 'Redux', 'Material UI', 'React Intl', 'SendGrid API', 'Mux Live stream API']}
+          }
+          stack={[
+            'Mongo DB',
+            'Express.js',
+            'React.js',
+            'Node.js',
+            'Socket.io',
+            'Redux',
+            'Material UI',
+            'React Intl',
+            'SendGrid API',
+            'Mux Live stream API',
+          ]}
         />
         <ProjectBox
           boxId="project-box-2"
+          projectTitle="Battleship Application"
+          projectLink="https://dmc-battleship.netlify.app/"
+          projectRepository="https://github.com/dmclavel/battleship"
+          content={
+            <span className="font-sans text-sm text-black dark:text-white-semi tracking-wide">
+              This is a Battleship application where a player hits a board square that may
+              contain a ship or not. A player wins when all the ships in the 10x10 board
+              are hit. This project focuses more on code structure, responsive layout,
+              unit test, and optimization: how to efficiently track every board state
+              without having the need to loop through nested list (2D array at the least
+              since this is a NxN board).
+            </span>
+          }
+          stack={['React', 'Typescript', 'Material UI', 'Jest', 'Parcel', 'Redux']}
+        />
+        <ProjectBox
+          boxId="project-box-3"
           projectTitle="Personal Portfolio (v1)"
           projectLink="https://dmclavel.github.io/"
           projectRepository="https://github.com/dmclavel/dmclavel.github.io"
-          content={(
+          content={
             <span className="font-sans text-sm text-black dark:text-white-semi tracking-wide">
               This is the first version of my portfolio which I made way back 2019. I
               integrated a lot of pre-made libraries in this portfolio like Typed.js, Owl
-              Carousel, Easy Pie Chart and Bootstrap. As this is the first iteration of
-              my portfolio, this is more of a playground to explore various libraries
-              and integrate other services like Hotjar, Firebase and Hubspot.
+              Carousel, Easy Pie Chart and Bootstrap. As this is the first iteration of my
+              portfolio, this is more of a playground to explore various libraries and
+              integrate other services like Hotjar, Firebase and Hubspot.
             </span>
-          )}
-          stack={['Javascript', 'HTML', 'CSS', 'jQuery', 'Bootstrap', 'Owl Carousel', 'Typed.js']}
+          }
+          stack={[
+            'Javascript',
+            'HTML',
+            'CSS',
+            'jQuery',
+            'Bootstrap',
+            'Owl Carousel',
+            'Typed.js',
+          ]}
         />
       </div>
     </>
